31e41d7b44efc912b3542f571fd97fa717a5760f
panforrest <guoqianp@gmail.com>
2016-12-04 05:38:10 -0500

    conact form post pass test by showing in console

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,12 @@ router.get('/about', function(req, res, next) {
   res.render('about', null);
 });
 
+router.post('/:action', function(req, res, next){
+	var action = req.params.action
+
+	if (action == 'contact'){        //if (action == 'post'){
+		console.log(req.body)     //console.log(params.body)     
+	} 
+})
+
 module.exports = router;
