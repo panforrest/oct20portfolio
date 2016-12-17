f2c9b176fd98b174e56fb66c11275334852169dc
panforrest <guoqianp@gmail.com>
2016-12-17 13:52:05 -0500

    perc route & content display

--- routes/index.js
+++ routes/index.js
@@ -10,12 +10,21 @@ router.get('/about', function(req, res, next) {
   res.render('about', null);
 });
 
-router.post('/:action', function(req, res, next){
+router.post('/:action', function(req, res, next){   //it is post not get
 	var action = req.params.action
 
 	if (action == 'contact'){        //if (action == 'post'){
 		console.log(req.body)     //console.log(params.body)     
 	} 
+})
+
+router.get('/project/:name', function(req, res, next){
+	var name = req.params.name 
+
+	if (name == name) {
+        res.render(name, null)		
+	}
+
 })
 
 module.exports = router;
