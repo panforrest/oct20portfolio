38b8f738e9e7fc7fa57bc1b582b2c9db60fd6493
panforrest <guoqianp@gmail.com>
2016-12-18 03:06:43 -0500

    sendgrid local deploy not work

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+require('dotenv').config()
 var routes = require('./routes/index');
 var sendgrid = require('./routes/sendgrid');
 var users = require('./routes/users');
-
+// require('dotenv').config()
 
 var app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
-var helper = require('sendgrid').mail;
-var from_email = new helper.Email('guoqianp@gmail.com');
-var to_email = new helper.Email('gzpgg3x@gmail.com');
+// var helper = require('sendgrid').mail;
+// var from_email = new helper.Email('info@thegridmedia.com');
+// var to_email = new helper.Email('dkwon@velocity360.io');
+// var subject = 'Hello World from the SendGrid Node.js Library!';
+// var content = new helper.Content('text/plain', 'Hello, Email!');
+// var mail = new helper.Mail(from_email, subject, to_email, content);
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about', null);
-});
+router.get('/about', function(req, res, next){
+	res.render('about', null)
+})
+
 
 router.get('/confirmation', function(req, res, next){
 	res.render('confirmation', null)
 })
 
-router.post('/:action', function(req, res, next){   //it is post not get
-	var action = req.params.action
 
-	if (action == 'contact'){        //if (action == 'post'){    NOT WORK 
-		console.log(req.body)     //console.log(params.body)    NOT WORK
+router.get('/project/:name', function(req, res, next){
+	var pages = ['perc', 'velocity360']
+	var name = req.params.name
+
+	if (pages.indexOf(name) == -1){
+		res.render('error', {message: 'Page does not exist. Please check your spelling.'})
+
+		return
+	}
+
+	res.render(name, null)
+	
+})
+
+
+router.post('/:action', function(req, res, next){
+	var action = req.params.action
+	
+	if (action == 'contact'){
+		console.log(req.body)
+
+		var helper = require('sendgrid').mail;
+		var from_email = new helper.Email('info@thegridmedia.com');
+		var to_email = new helper.Email('dkwon@velocity360.io');
 
 		var subject = req.body.subject;
 		var content = new helper.Content('text/plain', req.body.message);
 		var mail = new helper.Mail(from_email, subject, to_email, content);
 
-
-		//send email to yourself
-
-
-		// res.redirect('/confirmation')      //res.redirect('/confirmation', null) NOT WORK  
 		var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 		var request = sg.emptyRequest({
 		  method: 'POST',
@@ -45,48 +66,22 @@ router.post('/:action', function(req, res, next){   //it is post not get
 		  console.log(response.body);
 		  console.log(response.headers);
 
-			if (error){
-				res.json({
-					confirmation: 'fail',
-					message: error
-				})
+		  if (error){
+		  	res.json({
+		  		confirmation: 'fail',
+		  		message: error
+		  	})
 
-				return
-			}
+		  	return
+		  }
 
-            res.redirect('/confirmation')
-			// res.json({
-			// 	confirmation: 'success',
-			// 	response: response.body    //but wny?
-			// })
-
-		    return
-
-
+		  res.redirect('/confirmation')
+		  return
 
 		});
 
-
-
-	} 
-})
-
-
-
-router.get('/project/:name', function(req, res, next){
-    var pages = ['perc', 'velocity360']
-    
-	var name = req.params.name 
-
-	if (pages.indexOf(name) == -1){
-		res.render('error', {message: "Page does not exist, check your spelling"})
-        
-        return
 	}
 
-	if (name == name) {
-        res.render(name, null)		
-	}
 
 })
 
